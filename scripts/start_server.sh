3e8320fc4cad896d822f4ef9afdc678c70b4104b
stephencshelton <sshelton@sevatec.com>
2021-02-09 17:29:25 -0500

    Working now

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,14 +6,8 @@ export SteamAppId=892970
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name ${SRV_NAME} -port ${GAME_PORT} -world ${WORLD_NAME} -password ${SRV_PWD} -public ${PUBLIC} > /dev/null &
+./valheim_server.x86_64 -name ${SRV_NAME} -port ${GAME_PORT} -world ${WORLD_NAME} -password ${SRV_PWD} -public ${PUBLIC}
 
 export LD_LIBRARY_PATH=$templdpath
 
 echo "Server started"
-echo ""
-read -p "Press RETURN to stop server"
-echo 1 > server_exit.drp
-
-echo "Server exit signal set"
-echo "You can now close this terminal"
